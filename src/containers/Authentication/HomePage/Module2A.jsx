fc27fc6ca1890732b9e8d7c902ec90a1e4bb7ee7
spacessoradm <spacessor.ad@gmail.com>
2025-04-25 20:26:00 +0800

    Update Module2A.jsx

--- src/containers/Authentication/HomePage/Module2A.jsx
+++ src/containers/Authentication/HomePage/Module2A.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { ChevronDown, Check, ArrowUp } from "lucide-react"
 
 import UserHeader from '../../../components/UserHeader/index';
-import UserSidebar from '../../../components/UserSideBar/index.jsx';
+import UserSidebar from '../../../components/UserSidebar/index';
 
 const Module2A = () => {
     const userId = localStorage.getItem("profileId");
