cacc257cd5c730da0dd51c1f410157aee9929db3
spacessoradm <spacessor.ad@gmail.com>
2025-04-28 08:31:45 +0800

    Update TestByModulePhysicsQuestion.jsx

--- src/containers/Authentication/HomePage/TestByModulePhysicsQuestion.jsx
+++ src/containers/Authentication/HomePage/TestByModulePhysicsQuestion.jsx
@@ -4,7 +4,7 @@ import supabase from "../../../config/supabaseClient";
 import "./index.css";
 import { ChevronDown, Check, ArrowUp } from "lucide-react"
 import UserHeader from '../../../components/UserHeader/index';
-import UserSidebar from '../../../components/UserSideBar';
+import UserSidebar from '../../../components/UserSidebar/index';
 
 const TestByModulePhysicsQuestion = () => {
     const userId = localStorage.getItem("profileId");
