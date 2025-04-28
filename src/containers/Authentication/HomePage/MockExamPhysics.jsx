9a6b9f63ab3e4353ab216f51c4c360d8bfbcb139
spacessoradm <spacessor.ad@gmail.com>
2025-04-28 08:33:35 +0800

    Update MockExamPhysics.jsx

--- src/containers/Authentication/HomePage/MockExamPhysics.jsx
+++ src/containers/Authentication/HomePage/MockExamPhysics.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import supabase from "../../../config/supabaseClient";
 import UserHeader from '../../../components/UserHeader';
-import UserSidebar from '../../../components/UserSideBar';
+import UserSidebar from '../../../components/UserSidebar/index';
 
 const MockExamPhysics = () => {
   const navigate = useNavigate();
